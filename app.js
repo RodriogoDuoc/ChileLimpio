10012058686b6c66eecebdc666b5b72838b900a3
RodriogoDuoc <128237526+RodriogoDuoc@users.noreply.github.com>
2023-04-15 11:45:16 -0400

    validaciones

--- app.js
+++ app.js
@@ -1,25 +1,18 @@
+var rut = document.getElementById('rut');
+var apellidoPa = document.getElementById('apellidoPa');
+var apellidoMa = document.getElementById('apellidoMa');
 var nombre = document.getElementById('nombre');
-var password = document.getElementById('password');
+var fechaNac = document.getElementById('fechaNac');
+var edad = document.getElementById('edad');
+var genero = document.getElementById('genero');
+var email = document.getElementById('email');
+var celular = document.getElementById('celular');
+var motivacion = document.getElementById('motivacion');
+var profesion = document.getElementById('profesion');
+
+
 var error = document.getElementById('error');
 error.style.color = 'red';
-
-//function enviarFormulario(){
-   // console.log('Enviando Formulario....');
-
-   // var mensajesError = [];
-
-   // if(nombre.value === null || nombre.value === ''){
-    //    mensajesError.push('Ingresa tu nombre')
-   // }
-
-   // if(password.value === null || password.value === ''){
-       // mensajesError.push('Ingresa tu password')
-  //}  }
-
-       // error.innerHTM = mensajesError.join(', ');
-   // return false;
-//
-
 
 
 var form = document.getElementById('formulario');
@@ -27,6 +20,19 @@ var form = document.getElementById('formulario');
         evt.preventDefault();
         
         var mensajesError = [];
+
+    if(rut.value === null || rut.value === ''){
+            mensajesError.push('Ingrese su rut')
+    }
+
+    if(apellidoPa.value === null || apellidoPa.value === ''){
+        mensajesError.push('Apellido paterno')    
+    }   
+
+    if(apellidoMa.value === null || apellidoMa.value === ''){
+        mensajesError.push('Apellido materno')    
+    }
+
 
     if(nombre.value === null || nombre.value === ''){
         mensajesError.push('Ingresa tu nombre')
