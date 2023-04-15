ee2f0a57bcd0f483a9e8a3457a896c461c21910a
RodriogoDuoc <128237526+RodriogoDuoc@users.noreply.github.com>
2023-04-15 11:37:59 -0400

    requerimientos

---
+++ app.js
@@ -0,0 +1,41 @@
+var nombre = document.getElementById('nombre');
+var password = document.getElementById('password');
+var error = document.getElementById('error');
+error.style.color = 'red';
+
+//function enviarFormulario(){
+   // console.log('Enviando Formulario....');
+
+   // var mensajesError = [];
+
+   // if(nombre.value === null || nombre.value === ''){
+    //    mensajesError.push('Ingresa tu nombre')
+   // }
+
+   // if(password.value === null || password.value === ''){
+       // mensajesError.push('Ingresa tu password')
+  //}  }
+
+       // error.innerHTM = mensajesError.join(', ');
+   // return false;
+//
+
+
+
+var form = document.getElementById('formulario');
+    form.addEventListener('submit',function(evt){
+        evt.preventDefault();
+        
+        var mensajesError = [];
+
+    if(nombre.value === null || nombre.value === ''){
+        mensajesError.push('Ingresa tu nombre')
+    }
+
+    if(password.value === null || password.value === ''){
+        mensajesError.push('Ingresa tu password')
+    }
+
+        error.innerHTML = mensajesError.join(', ');    
+
+});
